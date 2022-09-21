1018bb6b5300d7bce5bb6539fb9f7ebccb75693c
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2022-09-21 17:50:02 +0300

    Done

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, Image, Alert, Button } from 'react-native';
+import React, { useState } from 'react';
+
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repo, setRepo] = useState([]);
+
+  
+
+
+  const getRepos = ()=> {
+      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then(response => response.json())
+      .then(data => setRepo(data.meals))
+      .catch(err => 
+          Alert.alert('Error', 'error')
+      )
+      setKeyword('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <FlatList style={styles.lista} 
+        data={repo}
+        renderItem={({item}) => 
+        <View>
+        <Text>{item.strMeal}</Text>
+        <Image style={styles.image}
+        source={{uri:item.strMealThumb}}
+        />
+        </View>    
+            }
+    />
+
+    <View>
+        <TextInput style ={styles.input}
+            value={keyword}
+            onChangeText={text => setKeyword(text)}
+        />
+        <Button 
+        title='Find' onPress={getRepos}
+    />
+    
+
+    
     </View>
+</View>
   );
 }
 
@@ -16,5 +56,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 30
+  },
+  lista: {
+    marginBottom: 10,
+    marginTop: 40
+  },
+  image: {
+    width: 380,
+    height: 150
+  },
+  input: {
+        
+    width: 150,
+    borderWidth:1,
+    borderColor: 'blue'
   },
 });
